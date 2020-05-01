dbefa5c50c8570805bb450aaef9a3aa07fb0e53c
jacobs67 <jared0801@gmail.com>
2020-05-01 13:51:09 -0700

    Added sounds on success/fail during deck testing as well as options for changing these sounds and other options (flip card, no animations) during test and review. Added a modal partial for confirmation.

--- public/javascripts/deck-review.js
+++ public/javascripts/deck-review.js
@@ -7,14 +7,25 @@ const skipOne = $('#skip-one');
 const prevButton = $('#prev');
 const nextButton = $('#next');
 const backButton = $('#back');
-const incorrectCount = $('#incorrect-count');
+const flipWarning = $('#flipWarning');
+const flipWarningButton = $('#flipWarningButton');
+const animationState = $('#animationState');
 let qNum = 0;
 let correct = 0;
 let questionSide = true;
 
 // Copies an array (deck), randomly shuffles it, then returns it
-function shuffleDeck(deck) {
-    let newDeck = [...deck];
+function shuffleDeck(deck, flipped=false) {
+    // Reinitialize stats
+    qNum = 0;
+    let newDeck;
+    if(flipped) {
+        newDeck = deck.map(card => {
+            return { answer: card.question, question: card.answer, image }
+        });
+    } else {
+        newDeck = [...deck];
+    }
     for(let i = newDeck.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i+1));
         [newDeck[i], newDeck[j]] = [newDeck[j], newDeck[i]];
@@ -36,33 +47,25 @@ function showCard(deck, round) {
     if(round === deck.length-1) backButton.show();
     else backButton.hide();
 
-    if(round >= deck.length && reviewForm.is(':visible')) {
-        // Deck is finished, display results
-        reviewForm.hide();
-        endForm.show();
-        if(correct >= deck.length / 2)
-            endForm.prepend(`<div class="alert alert-success">You scored ${correct} / ${deck.length} with ${incorrect} incorrect attempts.</div>`);
-        else
-            endForm.prepend(`<div class="alert alert-danger">You scored ${correct} / ${deck.length} with ${incorrect} incorrect attempts.</div>`);
-        return;
-    } else {
-        // Show question & image for a given round
-        if(deck[round].image && deck[round].image.url) {
-            image.attr('src', deck[round].image.url);
-            image.attr('alt', deck[round].image.question);
-        }
-        cardText.text(deck[round].question);
-        cardSide.text('Q');
-        questionSide = true;
+    // Show question & image for a given round
+    if(deck[round].image && deck[round].image.url) {
+        image.attr('src', deck[round].image.url);
+        image.attr('alt', deck[round].image.question);
     }
+    cardText.text(deck[round].question);
+    cardSide.text('Q');
+    questionSide = true;
     
 }
 
 function flipCard(deck, round) {
-    reviewForm.addClass('flipOutY');
+    let animated = reviewForm.hasClass('animated');
+    if(animated) reviewForm.addClass('flipOutY');
     setTimeout(() => {
-        reviewForm.removeClass('flipOutY');
-        reviewForm.addClass('flipInY');
+        if(animated) {
+            reviewForm.removeClass('flipOutY');
+            reviewForm.addClass('flipInY');
+        }
         if(questionSide) {
             cardText.text(deck[round].answer);
             cardSide.text('A');
@@ -71,7 +74,7 @@ function flipCard(deck, round) {
             cardSide.text('Q');
         }
         questionSide = !questionSide;
-    }, 500);
+    }, animated ? 500 : 0);
 }
 
 
@@ -108,6 +111,20 @@ $(document).ready(() => {
     prevButton.keypress(function(e) {
         if(e.which === 13) { // 13 maps to enter key
             showCard(shuffled, --qNum);
+        }
+    });
+    
+    flipWarningButton.click(function() {
+        flipWarning.modal('toggle');
+        shuffled = shuffleDeck(shuffled, true);
+        showCard(shuffled, 0);
+    });
+
+    animationState.change(function() {
+        if(animationState.val() === 'off') {
+            $('.animated').addClass('not-animated').removeClass('animated');
+        } else {
+            $('.not-animated').addClass('animated').removeClass('not-animated');
         }
     });
 });
--- public/javascripts/deck-test.js
+++ public/javascripts/deck-test.js
@@ -6,7 +6,18 @@ const image = $('#image');
 const testForm = $('#test-form');
 const endForm = $('#end-form');
 const skipOne = $('#skip-one');
+const audioState = $('#audioState');
+const successAudio = $('#successAudio');
+const successAudioSrc = successAudio[0];
+const successAudioState = $('#successAudioState');
+const failureAudio = $('#failureAudio');
+const failureAudioSrc = failureAudio[0];
+const failureAudioState = $('#failureAudioState');
 const incorrectCount = $('#incorrect-count');
+const flipWarning = $('#flipWarning');
+const flipWarningButton = $('#flipWarningButton');
+const animationState = $('#animationState');
+let animation = true;
 let ans = '';
 let qNum = 0;
 let correct = 0;
@@ -14,8 +25,23 @@ let incorrect = 0;
 let consecutiveFail = 0;
 
 // Copies an array (deck), randomly shuffles it, then returns it
-function shuffleDeck(deck) {
-    let newDeck = [...deck];
+function shuffleDeck(deck, flipped=false) {
+    // Reinit stats
+    correct = 0;
+    incorrect = 0;
+    consecutiveFail = 0;
+    qNum = 0;
+    rightAnswer.removeClass('show');
+    wrongAnswer.removeClass('show');
+    incorrectCount.removeClass('show');
+    let newDeck;
+    if(flipped) {
+        newDeck = deck.map(card => {
+            return { answer: card.question, question: card.answer, image }
+        });
+    } else {
+        newDeck = [...deck];
+    }
     for(let i = newDeck.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i+1));
         [newDeck[i], newDeck[j]] = [newDeck[j], newDeck[i]];
@@ -28,6 +54,9 @@ function showCard(deck, round) {
     // Clear any old image
     image.attr('src', '');
     image.attr('alt', '');
+    // Clear & focus input
+    ansInput.val('');
+    ansInput.focus();
     if(round >= deck.length && testForm.is(':visible')) {
         // Deck is finished, display results
         testForm.hide();
@@ -54,6 +83,7 @@ function testGuess(deck) {
     if(guess === ans) {
         // User guessed correctly
         consecutiveFail = 0;
+        playAudio(successAudioSrc);
         rightAnswer.addClass('show');
         // Display congrats message for 2s
         setTimeout(() => {
@@ -61,12 +91,13 @@ function testGuess(deck) {
         }, 2000);
         // Clear input and move to the next card
         wrongAnswer.removeClass('show');
-        ansInput.val('');
+        wrongAnswer.removeClass('shake');
         qNum++;
         correct++;
         showCard(deck, qNum);
     } else {
         // User guessed incorrectly
+        playAudio(failureAudioSrc);
         wrongAnswer.addClass('show');
         wrongAnswer.addClass('shake');
         rightAnswer.removeClass('show');
@@ -81,11 +112,25 @@ function testGuess(deck) {
                 skipOne.removeClass('show');
                 consecutiveFail = 0;
                 wrongAnswer.removeClass('show');
+                wrongAnswer.removeClass('shake');
             });
         }
         // Show total # incorrect guesses in top right
         incorrectCount.addClass('show');
-        incorrectCount.html(`<small><div class="animated fadeInUp">${incorrect}</div> incorrect</small>`);
+        if(animation) {
+            incorrectCount.html(`<small><div class="animated fadeInUp">${incorrect}</div> incorrect</small>`);
+        } else {
+            incorrectCount.html(`<small><div>${incorrect}</div> incorrect</small>`);
+        }
+    }
+}
+
+// Helper function makes sure the audio always restarts when needed
+function playAudio(audioSrc) {
+    if(audioSrc.paused) {
+        audioSrc.play();
+    } else {
+        audioSrc.currentTime = 0;
     }
 }
 
@@ -100,10 +145,67 @@ $(document).ready(() => {
         testGuess(shuffled);
     });
 
-    $('#ans-input').keypress(function(e) {
+    ansInput.keypress(function(e) {
         if(e.which === 13) { // 13 maps to enter key
             testGuess(shuffled);
         }
     });
+
+    successAudioState.change(function() {
+        const newFileOgg = `/audio/success/${successAudioState.val()}.ogg`;
+        const newFileMp3 = `/audio/success/${successAudioState.val()}.mp3`;
+        successAudio.children()[0].src = newFileOgg;
+        successAudio.children()[1].src = newFileMp3;
+        successAudioSrc.pause();
+        successAudioSrc.load();
+        successAudio.oncanplaythrough = playAudio(successAudioSrc);
+    });
+
+    failureAudioState.change(function() {
+        const newFileOgg = `/audio/failure/${failureAudioState.val()}.ogg`;
+        const newFileMp3 = `/audio/failure/${failureAudioState.val()}.mp3`;
+        failureAudio.children()[0].src = newFileOgg;
+        failureAudio.children()[1].src = newFileMp3;
+        failureAudioSrc.pause();
+        failureAudioSrc.load();
+        failureAudio.oncanplaythrough = playAudio(failureAudioSrc);
+    });
+
+    audioState.change(function() {
+        if(audioState.val() === 'off') {
+            failureAudioState.attr('disabled', 'true');
+            successAudioState.attr('disabled', 'true');
+            failureAudio[0].muted = true;
+            successAudio[0].muted = true;
+        } else {
+            failureAudioState.removeAttr('disabled');
+            successAudioState.removeAttr('disabled');
+            failureAudio[0].muted = false;
+            successAudio[0].muted = false;
+        }
+    });
+
+    flipWarningButton.click(function() {
+        flipWarning.modal('toggle');
+        shuffled = shuffleDeck(shuffled, true);
+        showCard(shuffled, 0);
+    });
+
+    animationState.change(function() {
+        if(animationState.val() === 'off') {
+            $('.animated').addClass('not-animated').removeClass('animated');
+            animation = false;
+        } else {
+            $('.not-animated').addClass('animated').removeClass('not-animated');
+            animation = true;
+        }
+    });
+
+    wrongAnswer.click(function() {
+        wrongAnswer.removeClass('show');
+        wrongAnswer.removeClass('shake');
+    });
+
+
 });
 
